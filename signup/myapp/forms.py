96eaf8421e0e21df9bdb358781c0ffc4dca01f36
maskottchentech <54236108+maskottchentech@users.noreply.github.com>
2019-08-25 01:15:29 +0530

    Update forms.py

--- signup/myapp/forms.py
+++ signup/myapp/forms.py
@@ -9,4 +9,4 @@ class SignupUser(UserCreationForm):
 
 	class Meta:
 		model = User
-		fields = ('username','first_name','last_name','password1','password2',)
+		fields = ('username','first_name','last_name','email','password1','password2',)
