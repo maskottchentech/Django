8bdb86b3bfa4cdfd905506d10ad6d3edcdc0d546
maskottchentech <54236108+maskottchentech@users.noreply.github.com>
2020-04-15 00:23:41 +0530

    Update settings.py

--- DJangoemail/djangoemail/settings.py
+++ DJangoemail/djangoemail/settings.py
@@ -124,5 +124,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'theshubhankertiwari@gmail.com'
-EMAIL_HOST_PASSWORD = 'llmsdpwqqoglnknt'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your password'
