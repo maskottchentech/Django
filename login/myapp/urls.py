ec39461b3bf460dfa36e4ed09ddee07666a272ba
maskottchentech <54236108+maskottchentech@users.noreply.github.com>
2020-04-24 14:14:42 +0530

    Update urls.py

--- login/myapp/urls.py
+++ login/myapp/urls.py
@@ -5,7 +5,7 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('signup/', views.signupform,name='signup'),
-    path('login/', views.UserLogin,name='login'),
+    path('login/', views.Userlogin,name='login'),
     path('profile/', views.userprofile,name='profile'),
     path('home/', views.Homepage,name='home'),
 
