6206ada3a07a55b75cba168a849c4d052de6652d
maskottchentech <54236108+maskottchentech@users.noreply.github.com>
2020-04-24 14:15:02 +0530

    Update forms.py

--- login/myapp/forms.py
+++ login/myapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from .models import ArticlePkandSlug
+
 
 class SignupUser(UserCreationForm):
 	first_name = forms.CharField(max_length=100,required = True)
