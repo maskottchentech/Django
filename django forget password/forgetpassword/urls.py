d08c253aed41b48e0562af4088d09f9a54a7045d
maskottchentech <54236108+maskottchentech@users.noreply.github.com>
2020-05-22 01:50:03 +0530

    Add files via upload

---
+++ django forget password/forgetpassword/urls.py
@@ -0,0 +1,32 @@
+"""forgetpassword URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from testapp import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('signup/', views.signupform,name='signup'),
+    path('login/', views.Userlogin,name='login'),
+    path('home/', views.Homepage,name='home'),
+    path('password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+   
+]
